e60d95aa8d0a5fae8585ee89b536cc8162a202d3
MagnavexBytes <dmitriykh13@gmail.com>
2026-05-23 14:08:23 +0500

    Update Lab0

--- Lab0/task1.jl
+++ Lab0/task1.jl
@@ -14,3 +14,24 @@ end
 for i in 1:N
     println(A[i,:])
 end
+
+# function create_tridiagonal_matrix(N)
+#     A = zeros(Int, N, N)
+    
+#     for i in 1:N
+#         for j in 1:N
+#             if i == j
+#                 A[i, j] = 2
+#             elseif abs(i - j) == 1
+#                 A[i, j] = 1
+#             end
+#         end
+#     end
+    
+#     return A
+# end
+
+# N = 5
+# matrix = create_tridiagonal_matrix(N)
+
+# display(matrix)
